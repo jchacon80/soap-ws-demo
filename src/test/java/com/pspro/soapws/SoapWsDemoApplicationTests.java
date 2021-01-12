6752f2056ed25799ae59ff6d25463069afa8f5df
jchac <jchac@DESKTOP-OV53H8G>
2021-01-12 16:36:46 +0100

    primera subida

---
+++ src/test/java/com/pspro/soapws/SoapWsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pspro.soapws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
